071c7803e8fda1bf8ce4a01293e4c18f12054043
Minh Lam Su <kelvincoder@gmail.com>
2019-08-06 09:49:40 +0700

    Add root mutations GraphQL

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,7 +1,37 @@
+const posts = [
+  {
+    id: 2,
+    text: "Minh rat la dep trai",
+    user: {
+      avatar: "/uploads/avatar1.png",
+      username: "minhlam"
+    }
+  },
+  {
+    id: 1,
+    text: "Minh that su rat la dep trai",
+    user: {
+      avatar: "/uploads/avatar2.png",
+      username: "kelvinlam"
+    }
+  }
+];
+
 const resolvers = {
   RootQuery: {
     posts(root, args, context) {
-      return [];
+      return posts;
+    }
+  },
+  RootMutation: {
+    addPost(root, { post, user }, context) {
+      const postObject = {
+        ...post,
+        user,
+        id: post.length + 1
+      };
+      posts.push(postObject);
+      return postObject;
     }
   }
 };
